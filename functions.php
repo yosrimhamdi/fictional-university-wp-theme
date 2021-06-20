42f5f03f5a6c4417f2800b4125381fddd29e5b58
yosrimhamdi <bavary1515@gmail.com>
2021-06-20 13:34:45 +0100

    initial commit.

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function load_files() {
+  wp_enqueue_style('main-styles', get_stylesheet_uri());
+  wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('typography', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_script('scripts', get_theme_file_uri() . '/assets/js/scripts.js', null, '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'load_files');
+
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
---
+++ single.php
@@ -0,0 +1,4 @@
+<?php get_header() ?>
+<?php echo the_title() ?>
+<?php echo the_content() ?>
+<?php get_footer() ?>
