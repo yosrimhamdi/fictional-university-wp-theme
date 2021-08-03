598b9e3b1b9ecb6d2fd69178ccf8237f4169d41e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-03 11:10:04 +0100

    added a makefile.

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 <div class="page-banner">
     <div 
       class="page-banner__bg-image"
-      style="background-image: url(<?php echo get_theme_file_uri() ?>/assets/images/ocean.jpg);">
+      style="background-image: url(<?php echo get_theme_file_uri('/assets/images/ocean.jpg') ?>);">
     </div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title() ?></h1>
