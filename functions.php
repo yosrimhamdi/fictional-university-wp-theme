4151e15a76b288a17d31fa2a5f61d2f8eedd20da
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-03 10:30:58 +0100

    the right way to use get_theme_file_uri function.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function load_files() {
   wp_enqueue_style('main-styles', get_stylesheet_uri());
   wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('typography', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_script('scripts', get_theme_file_uri() . '/assets/js/scripts.js', null, '1.0', true);
+  wp_enqueue_script('scripts', get_theme_file_uri('/assets/js/scripts.js'), null, '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'load_files');
