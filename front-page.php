7c6cbb44bb6f034808ee28ce2be0601c0c79c9fb
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-10 18:55:58 +0100

    connecting the bundled js and css files to the wordpress theme.

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri() ?>/assets/images/library-hero.jpg);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri() ?>/src/images/library-hero.jpg);"></div>
   <div class="page-banner__content container t-center c-white">
     <h1 class="headline headline--large">Welcome!</h1>
     <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -65,7 +65,7 @@
 <div class="hero-slider">
   <div data-glide-el="track" class="glide__track">
     <div class="glide__slides">
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/assets/images/bus.jpg);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/src/images/bus.jpg);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -74,7 +74,7 @@
           </div>
         </div>
       </div>
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/assets/images/apples.jpg);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/src/images/apples.jpg);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -83,7 +83,7 @@
           </div>
         </div>
       </div>
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/assets/images/bread.jpg);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri() ?>/src/images/bread.jpg);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">Free Food</h2>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class="page-banner">
     <div 
       class="page-banner__bg-image"
-      style="background-image: url(<?php echo get_theme_file_uri('/assets/images/ocean.jpg') ?>);">
+      style="background-image: url(<?php echo get_theme_file_uri('/src/images/ocean.jpg') ?>);">
     </div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title">our top 10 posts</h1>
