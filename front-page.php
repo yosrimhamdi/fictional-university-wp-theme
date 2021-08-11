1515f7aed453fb6a1f65764554796ba09ba3745c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-11 18:49:56 +0100

    added a permalink.

--- front-page.php
+++ front-page.php
@@ -29,7 +29,7 @@
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny">
-              <a href="#"><?php the_title() ?></a>
+              <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
             </h5>
             <p><?php echo wp_trim_words(get_the_content(), 18) ?>. <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
           </div>
