e0dd16e21c1ad3a9dd8aaa4fd3159127f23a6f79
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-12 14:47:46 +0100

    added dynamic links and excerpts at the font-page.php.

--- front-page.php
+++ front-page.php
@@ -31,13 +31,18 @@
             <h5 class="event-summary__title headline headline--tiny">
               <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
             </h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18) ?>. <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+            <p>
+            <?php 
+              echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
+            ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
           </div>
         </div>
       <?php
         }
+
+        wp_reset_postdata();
       ?>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
     </div>
   </div>
   <div class="full-width-split__two">
