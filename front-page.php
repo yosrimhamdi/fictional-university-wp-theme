d28d8020d2172bb2654c404e919cb691d480d49e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-12 15:18:17 +0100

    getting the costum field value.

--- front-page.php
+++ front-page.php
@@ -21,11 +21,13 @@
 
         while ($events->have_posts()) {
           $events->the_post();
+
+          $date = new DateTime(get_field('event_date'));
       ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-            <span class="event-summary__month"><?php the_time('M') ?></span>
-            <span class="event-summary__day"><?php the_time('d') ?></span>
+            <span class="event-summary__month"><?php echo $date->format('M') ?></span>
+            <span class="event-summary__day"><?php echo $date->format('d') ?></span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny">
