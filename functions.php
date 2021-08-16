6a8ea47fdbfa25ccf39dd339e16a69001cd83c4e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-16 14:53:47 +0100

    changing the default wp Query on programs.

--- functions.php
+++ functions.php
@@ -20,18 +20,32 @@ function university_features() {
 add_action('after_setup_theme', 'university_features');
 
 function adjust_query($query) {
-  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-    $query->set('meta_key', 'event_date');
-    $query->set('orderby', 'meta_value_num');
-    $query->set('order', 'asc');
-    $query->set('meta_query', [
-      [
-       'key' => 'event_date',
-       'compare' => '>=',
-       'value' => date('Ymd'),
-       'type' => 'numeric'
-      ]
-     ]);
+  if (is_admin() || !$query->is_main_query()) {
+    return 0;   
+  }
+
+  switch (true) {
+    case is_post_type_archive('event') : {
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'asc');
+      $query->set('meta_query', [
+        [
+         'key' => 'event_date',
+         'compare' => '>=',
+         'value' => date('Ymd'),
+         'type' => 'numeric'
+        ]
+       ]);
+      
+      break;
+    }
+
+    case is_post_type_archive('program') : {
+      $query->set('posts_per_page', -1);
+      $query->set('orderby', 'title');
+      $query->set('order', 'asc');
+    }
   }
 }
 
