94c33abc21e7abbff5ae9165f68f8ebe01aa300c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-16 15:34:33 +0100

    displaying the related programs if there is any.

--- single-event.php
+++ single-event.php
@@ -26,20 +26,26 @@
         </p>
       </div>
       <div class="generic-content">
-      <?php echo the_content(); ?>
+      <?php 
+        echo the_content();
+        
+        $relatedPrograms = get_field('related_programs');
 
-      <div>related programs:</div>
-        <?php
-          $relatedPrograms = get_field('related_programs');
+          if ($relatedPrograms) {
+            echo '<hr class="section-break" />';
+            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+            echo '<ul class="link-list min-list">';
 
-          foreach ($relatedPrograms as $program) {
+            foreach ($relatedPrograms as $program) {
+              ?>
+                  <li div><a href="<?php  echo get_the_permalink($program) ?>">
+                    <?php echo get_the_title($program) ?></a>
+                  </li>
+              <?php
+            } 
+          }         
         ?>
-            <div div><a href="<?php  echo get_the_permalink($program) ?>">
-              <?php echo get_the_title($program) ?></a>
-            </div>
-        <?php
-          }
-        ?>
+      </ul>
       </div>
     </div>
 <?php
