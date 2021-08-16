c916e27bb466dd8226954414eaf896c2862e99b9
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-16 14:24:43 +0100

    added a new post type called programs.

---
+++ archive-program.php
@@ -0,0 +1,28 @@
+<?php get_header() ?>
+<div class="page-banner">
+    <div 
+      class="page-banner__bg-image"
+      style="background-image: url(<?php echo get_theme_file_uri('/src/images/ocean.jpg') ?>);">
+    </div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">All Programs</h1>
+      <div class="page-banner__intro">
+        <p>Bla Bla Bla.</p>
+      </div>
+    </div>  
+</div>
+<div class="container container--narrow page-section">
+  <ul class="link-list min-list">
+  <?php 
+    while(have_posts()) { 
+      the_post();
+      $eventDate = new DateTime(get_field('event_date'));
+  ?>
+      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  <?php 
+    } 
+    echo paginate_links(); 
+  ?>
+  </ul>
+</div>
+<?php get_footer() ?>
