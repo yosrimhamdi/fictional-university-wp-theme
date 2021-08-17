9d23b0e215d6b0a3efeba8579ef22214e4b1a64b
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-17 13:09:50 +0100

    added an event handler.

--- autoreload.js
+++ autoreload.js
@@ -1,7 +1,7 @@
 var bs = require('browser-sync').create();
 
 bs.watch('*.php', function (event, file) {
-  if (event === 'change') {
+  if (event === 'change' || event === 'add') {
     bs.reload();
   }
 });
