6aef5b6d934f1749db8ddd8cb5bc93f187bf0f9a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-21 14:20:30 +0100

    using the get_template_part function.

--- page-old-events.php
+++ page-old-events.php
@@ -24,21 +24,8 @@
     <?php
       while ($oldEvents->have_posts()) {
         $oldEvents->the_post();
-        $eventDate = new DateTime(get_field('event_date'));
-    ?>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-            <span class="event-summary__month"><?php echo $eventDate->format('M') ?></span>
-            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny">
-              <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-            </h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18) ?>. <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-          </div>
-        </div>
-    <?php
+       
+        get_template_part('templates/event');
       }
 
       echo paginate_links([
--- single-program.php
+++ single-program.php
@@ -89,24 +89,7 @@
         while ($events->have_posts()) {
           $events->the_post();
 
-          $date = new DateTime(get_field('event_date'));          
-      ?>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-            <span class="event-summary__month"><?php echo $date->format('M') ?></span>
-            <span class="event-summary__day"><?php echo $date->format('d') ?></span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny">
-              <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-            </h5>
-            <p>
-            <?php 
-              echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
-            ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
-          </div>
-        </div>
-      <?php
+          get_template_part('templates/event');
         }
         wp_reset_postdata();
       ?>
