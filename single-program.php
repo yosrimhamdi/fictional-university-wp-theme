2fbd6dfca98ba7c9af66a8967b7bafd5c0fb838b
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-26 17:47:45 +0100

    showing related campuses on each program.

--- single-program.php
+++ single-program.php
@@ -16,83 +16,103 @@
         </p>
       </div>
       <div class="generic-content">
-      <?php echo the_content(); ?>
-      <?php
-        $eventTitle = get_the_title();
-
-        $professors = new WP_Query([
-          'post_type' => 'professor',
-          'posts_per_page' => -1,
-          'orderby' => 'title',
-          'order' => 'asc',
-          'meta_query' => [
-            [
-              'key' => 'related_programs',
-              'compare' => 'LIKE',
-              'value' => '"' . get_the_ID() . '"'
-            ]
-          ]
-        ]);
-
-        if ($professors->have_posts()) { 
-          echo '<hr class="section-break"/>';
-          echo '<h2 class="headline headline--medium">' . get_the_title() .  ' Professor(s)</h2>';
-        }
-       
-        echo '<ul class="professor-cards">';
-        
-        while ($professors->have_posts()) {
-          $professors->the_post();
-
-        ?>
-          <li class="professor-card__list-item">
-            <a class="professor-card" href="<?php the_permalink() ?>">
-             <img class="professor-card__image" src="<?php the_post_thumbnail_url('professor-landscape'); ?>" />
-             <span class="professor-card__name"><?php the_title(); ?></span>
-            </a>
-          </li>
+        <?php echo the_content(); ?>
         <?php
-        }
-        echo '</ul>';
+          $eventTitle = get_the_title();
 
-        wp_reset_postdata();
-
-        $events = new WP_Query([
-          'post_type' => 'event',
-          'posts_per_page' => 2,
-          // ordring here
-          'meta_key' => 'event_date',
-          'orderby' => 'meta_value_num',
-          'order' => 'asc',
-          // filtering here
-          'meta_query' => [
-            [
-              'key' => 'event_date',
-              'compare' => '>=',
-              'value' => date('Ymd'),
-              'type' => 'numeric'
-            ],
-            [
-              'key' => 'related_programs',
-              'compare' => 'LIKE',
-              'value' => '"' . get_the_ID() . '"'
+          $professors = new WP_Query([
+            'post_type' => 'professor',
+            'posts_per_page' => -1,
+            'orderby' => 'title',
+            'order' => 'asc',
+            'meta_query' => [
+              [
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              ]
             ]
-          ]
-        ]);
-        
+          ]);
 
-        if ($events->have_posts()) { 
-          echo '<hr class="section-break"/>';
-          echo "<h2 class='headline headline--medium'>Upcoming " . get_the_title() .  " Events</h2>";  
-        }
+          if ($professors->have_posts()) { 
+            echo '<hr class="section-break"/>';
+            echo '<h3 class="headline headline--medium">' . get_the_title() .  ' Professor(s)</h3>';
+          }
         
-        while ($events->have_posts()) {
-          $events->the_post();
+          echo '<ul class="professor-cards">';
+          
+          while ($professors->have_posts()) {
+            $professors->the_post();
 
-          get_template_part('templates/event');
-        }
-        wp_reset_postdata();
-      ?>
+          ?>
+            <li class="professor-card__list-item">
+              <a class="professor-card" href="<?php the_permalink() ?>">
+              <img class="professor-card__image" src="<?php the_post_thumbnail_url('professor-landscape'); ?>" />
+              <span class="professor-card__name"><?php the_title(); ?></span>
+              </a>
+            </li>
+          <?php
+          }
+          echo '</ul>';
+
+          wp_reset_postdata();
+
+          $events = new WP_Query([
+            'post_type' => 'event',
+            'posts_per_page' => 2,
+            // ordering here
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'asc',
+            // filtering here
+            'meta_query' => [
+              [
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => date('Ymd'),
+                'type' => 'numeric'
+              ],
+              [
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              ]
+            ]
+          ]);
+          
+
+          if ($events->have_posts()) { 
+            echo '<hr class="section-break"/>';
+            echo "<h3 class='headline headline--medium'>Upcoming " . get_the_title() .  " Events</h3>";  
+          }
+          
+          while ($events->have_posts()) {
+            $events->the_post();
+
+            get_template_part('templates/event');
+          }
+          wp_reset_postdata();
+
+          $relatedCampuses = get_field('related_campuses');
+
+          if ($relatedCampuses) {
+            echo '<hr class="section-break"/>';
+            echo '<h3 class="headline headline--medium">' . get_the_title() . ' Is Available At These Campus(es)</h3>';
+            echo '<ul class="min-list link-list">';
+          
+            foreach ($relatedCampuses as $campus):
+            ?>
+              <li>
+                <a href="<?php echo get_the_permalink($campus)?>">
+                  <?php echo get_the_title($campus) ?>
+                </a>
+              </li>
+            <?php
+            endforeach;
+
+            echo '</ul>';
+          }
+        ?>
       </div>
   </div>
 <?php 
