685cd750c1f249a9392e89aaa70fd3f17d99eec8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-26 16:49:09 +0100

    manual code formatting.

--- single-campus.php
+++ single-campus.php
@@ -9,17 +9,20 @@ while (have_posts()) {
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus') ?>">
+          <a 
+            class="metabox__blog-home-link" 
+            href="<?php echo get_post_type_archive_link('campus') ?>"
+          >
             <i class="fa fa-home" aria-hidden="true"></i>
             All Campuses
           </a>
-          <span class="metabox__main"><?php the_title(); ?></span>
+          <span class="metabox__main"><?php the_title() ?></span>
         </p>
       </div>
       <div class="generic-content">
-      <?php the_content(); ?>
+      <?php the_content() ?>
       <div class="acf-map">
-        <?php the_map($location)?>
+        <?php the_map($location) ?>
       </div>
       </div>
     </div>
@@ -27,4 +30,4 @@ while (have_posts()) {
 }
 ?>
 
-<?php get_footer()?>
+<?php get_footer() ?>
