57bffe9bb3030014a4062aa9e697e74966f9ecf0
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-28 17:00:53 +0100

    basic search overlay behaviour.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import SearchOverlay from './SearchOverlay';
 
-const App = () => (
-  <div>
-    <SearchOverlay />
-  </div>
-);
+const App = () => <SearchOverlay />;
 
 export default App;
--- src/components/SearchOverlay.js
+++ src/components/SearchOverlay.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
 
-const SearchOverlay = () => (
-  <div class="search-overlay">
-    <div class="search-overlay__top">
-      <div class="container">
-        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-        <input
-          type="text"
-          class="search-term"
-          placeholder="What are you looking for?"
-          id="search-term"
-        />
-        <i
-          class="fa fa-window-close search-overlay__close"
-          aria-hidden="true"
-        ></i>
+import Input from './Input';
+
+const SearchOverlay = () => {
+  const [active, setActive] = useState(false);
+  const closeButton = useRef();
+
+  useEffect(() => {
+    document.querySelector('.open').onclick = () => setActive(true);
+  }, []);
+
+  const className = classnames({
+    'search-overlay': true,
+    'search-overlay--active': active,
+  });
+
+  return (
+    <div className={className}>
+      <div className="search-overlay__top">
+        <div className="container">
+          <i
+            className="fa fa-search search-overlay__icon"
+            aria-hidden="true"
+          ></i>
+          <Input />
+          <i
+            className="fa fa-window-close search-overlay__close"
+            aria-hidden="true"
+            ref={closeButton}
+            onClick={() => setActive(false)}
+          ></i>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SearchOverlay;
