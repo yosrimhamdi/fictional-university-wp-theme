7ebc9b3293d0bf1ceed977590bc3239f6618d055
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-31 17:44:55 +0100

    added the costum hook into a compoenent body.

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PostTypeResult from './PostTypeResult';
-import useFetchResults from './useFetchResults';
+import fictionalUniversity from '../apis/fictionalUniversity';
+import postTypes from '../postTypes';
 
 const SearchResults = ({ term, setTerm }) => {
-  const results = useFetchResults(term, setTerm);
+  const [results, setResults] = useState({});
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      if (!term) {
+        return;
+      }
+
+      const promises = [];
+
+      for (const postType of postTypes) {
+        const promise = fictionalUniversity.get('/' + postType, {
+          params: {
+            search: term,
+          },
+        });
+
+        promises.push(promise);
+      }
+
+      const results = await Promise.all(promises);
+
+      const state = {};
+
+      results.forEach((result, i) => (state[postTypes[i]] = result.data));
+
+      setResults(state);
+      setTerm('');
+    };
+
+    const timerId = setTimeout(fetchPosts, 500);
+
+    return () => clearTimeout(timerId);
+  }, [term]);
   if (term) {
     return <div className="spinner-loader" />;
   }
