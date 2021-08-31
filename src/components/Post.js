2a5e54981f9ebfd7035b77858e6fd1db5704a81e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-31 17:40:22 +0100

    [fix] eslint error.

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Post = ({ post }) => (
   <li>
-    <a href={post.link} target="_blank">
+    <a href={post.link} target="_blank" rel="noreferrer">
       {post.title.rendered}
     </a>
   </li>
