5882d83f053a9f5cd95786c3f023292c879c534e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-08 22:16:26 +0100

    response contains author name and post type.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const Post = ({ post }) => (
-  <li>
-    <a href={post.url}>{post.title}</a>
-  </li>
-);
+const Post = ({ post }) => {
+  const { title, url, postType, author } = post;
+
+  if (postType == 'post' || postType == 'page') {
+    return (
+      <li>
+        <a href={url}>
+          {title} by {author}
+        </a>
+      </li>
+    );
+  }
+
+  return (
+    <li>
+      <a href={url}>{title}</a>
+    </li>
+  );
+};
 export default Post;
