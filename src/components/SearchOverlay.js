0fb5974748eba4131d2f0941a2015729019d6f9a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-23 12:57:48 +0100

    Default search overlay state.

--- src/components/SearchOverlay.js
+++ src/components/SearchOverlay.js
@@ -4,8 +4,8 @@ import classnames from 'classnames';
 import SearchResults from './SearchResults';
 
 const SearchOverlay = () => {
-  const [active, setActive] = useState(true);
-  const [term, setTerm] = useState('lorem');
+  const [active, setActive] = useState(false);
+  const [term, setTerm] = useState('');
 
   useEffect(() => {
     document.querySelector('.open .fa-search').onclick = () => setActive(true);
